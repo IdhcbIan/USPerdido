585d591a7a24cdc694254515a5170589e812f639
NicolasRossoni <nicolas.o.rossoni@gmail.com>
2024-06-13 04:22:21 -0300

    adding style

--- public/main.js
+++ public/main.js
@@ -1,41 +1,42 @@
+// ---------- Menu ---------- //
+let menu_buttons = [document.getElementById('home'),
+                    document.getElementById('info'),
+                    document.getElementById('comu'),
+                    document.getElementById('cont'),];
 
-// ---------- Boxes ---------- //
+menu_buttons[0].addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('logo').scrollIntoView({behavior: 'smooth'});
+});
+menu_buttons[1].addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('intro').scrollIntoView({behavior: 'smooth'});
+});
+menu_buttons[2].addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('communities').scrollIntoView({behavior: 'smooth'});
+});
+menu_buttons[3].addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('contact').scrollIntoView({behavior: 'smooth'});
+});
 
-document.querySelectorAll('.box').forEach(box => {
-    box.addEventListener('mouseover', () => {
-        box.classList.add('hover_effect_box');
-    });
-    box.addEventListener('mouseout', () => {
-        box.classList.remove('hover_effect_box');
-    });
-})
+// ---------- Introduction ---------- //
+
+document.addEventListener('DOMContentLoaded', function() {
+    let cards = document.getElementById('cards');
+    let intro = document.getElementById('intro');
+    
+    let observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                cards.classList.add('slide_right');
+                observer.unobserve(intro);
+            } else {
+            }
+        });
+    }, {threshold: 0.5});
+    observer.observe(intro);
+});
 
 // ---------- Communities ---------- //
-
-let  community1 = document.getElementById('community1');
-let  community2 = document.getElementById('community2');
-let  community3 = document.getElementById('community3');
-let  community4 = document.getElementById('community4');
-let  community5 = document.getElementById('community5');
-let communities = [community1, community2, community3, community4, community5];
-
-communities.forEach(community => {communities_animations(community);});
-
-function communities_animations(community) {
-    community.addEventListener('click', function(event) {
-        event.preventDefault();
-        window.location.href = 'c1/c1.html';
-    });
-
-    community.addEventListener('mouseover', function(event) {
-        event.preventDefault();
-
-        community.classList.add('hover_effect');
-    });
-
-    community.addEventListener('mouseleave', function(event) {
-        event.preventDefault();
-
-        community.classList.remove('hover_effect');
-    });
-}
